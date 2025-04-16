7c7bb3fc8921c48238024beabfbafe72cce0b363
aefrivern <2771503468@qq.com>
2025-04-16 19:43:11 +0800

    fix: update base path for assets and links to include '/Astro-blog'
    - Changed the base path in astro.config.mjs to '/Astro-blog' for correct asset loading.
    - Updated image paths in BaseHead.astro, blog posts, and styles to reflect the new base path.
    - Adjusted internal links in Header.astro to point to the correct routes.
    - Ensured all hero images in blog posts reference the updated path.
    - Modified global styles to load fonts from the new base path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aefrivern.github.io',
-  base: 'Astro-blog',
+  base: '/Astro-blog',
   integrations: [mdx(), sitemap()],
 });
